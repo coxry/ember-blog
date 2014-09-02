70b56e5a5179f1ab75c58cbd50c66c0c49c72c26
Raymond Cox <rj.cox101@gmail.com>
2014-09-01 21:13:26 -0400

    Update to latest ember-cli version

--- ember/config/environment.js
+++ ember/config/environment.js
@@ -18,21 +18,16 @@ module.exports = function(environment) {
     }
   };
 
-  ENV['simple-auth'] = {
-    authorizer: 'authorizer:rails',
-    store: 'simple-auth-session-store:local-storage'
-  };
-
   if (environment === 'development') {
-    // LOG_MODULE_RESOLVER is needed for pre-1.6.0
-    ENV.LOG_MODULE_RESOLVER = true;
-
-    ENV.APP.LOG_RESOLVER = true;
+    // ENV.APP.LOG_RESOLVER = true;
     ENV.APP.LOG_ACTIVE_GENERATION = true;
-    ENV.APP.LOG_MODULE_RESOLVER = true;
     // ENV.APP.LOG_TRANSITIONS = true;
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
     ENV.APP.LOG_VIEW_LOOKUPS = true;
+  }
+
+  if (environment === 'test') {
+    ENV.baseURL = '/'; // Testem prefers this...
   }
 
   if (environment === 'production') {
