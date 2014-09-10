d5e0d0914de441e219576f5c1604bdfff82bbccb
Raymond Cox <rj.cox101@gmail.com>
2014-09-10 19:19:03 -0400

    Remove .json suffix (originally for stubs)
    It was fixed in ember-cli

--- ember/app/adapters/application.js
+++ ember/app/adapters/application.js
@@ -1,10 +1,5 @@
 import DS from 'ember-data';
 
 export default DS.ActiveModelAdapter.extend({
-  suffix: '.json',
   namespace: 'api/v1',
-
-  pathForType: function(type) {
-    return this._super(type) + this.get('suffix');
-  }
 });
--- ember/server/routes/posts.js
+++ ember/server/routes/posts.js
@@ -1,5 +1,5 @@
 module.exports = function(app) {
-  app.get('/api/v1/posts.json', function(req, res) {
+  app.get('/api/v1/posts', function(req, res) {
     res.send({posts:[{id: 1, title: 'hello', body: 'body'}, {id: 2, title: 'Test Post', body: 'woot'}]});
   });
 };
